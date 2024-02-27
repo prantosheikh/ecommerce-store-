3a7c0afd53ff019dd466b4b5f65b02f188c1e6c9
Pranto Sheikh <prantosheikh834@gmail.com>
2024-02-28 00:59:01 +0600

    I am still working on it

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import getBillboard from '@/actions/get-billboard';
+import Billboard from '@/components/billboard';
 
-const HomePage = () => {
+export const revalidate = 0;
+const HomePage = async () => {
+
+   const billboard = await getBillboard("f5310f64-22da-49be-954c-b0bb3610a2f2")
    return (
-      <div>
-         Hello Store
+      <div className='space-y-10 pb-10'>
+         <Billboard data={billboard} />
       </div>
    );
 };
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,6 +3,10 @@ import Container from '@/components/ui/container';
 import Link from 'next/link';
 import MainNav from '@/components/main-nav';
 import getCategories from '@/actions/get-categories';
+// import { revalidatePath } from 'next/cache';
+import NavbarActions from './navbar-actions';
+
+export const revalidate = 0;
 
 const Navbar = async () => {
 
@@ -16,6 +20,7 @@ const Navbar = async () => {
                   <p className='font-bold text-xl'>STORE</p>
                </Link>
                <MainNav data={categories} />
+               <NavbarActions />
             </div>
          </Container>
       </div>
