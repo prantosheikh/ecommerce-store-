c7c00383b2f5f73fa9c12909425fe53abd2650c2
Pranto sheikh <prantosheikh834@gmail.com>
2024-03-02 20:06:35 +0600

    office work

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,15 +1,25 @@
-import getBillboard from '@/actions/get-billboard';
-import Billboard from '@/components/billboard';
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
 
 export const revalidate = 0;
 const HomePage = async () => {
-
-   const billboard = await getBillboard("f5310f64-22da-49be-954c-b0bb3610a2f2")
-   return (
-      <div className='space-y-10 pb-10'>
-         <Billboard data={billboard} />
-      </div>
-   );
+	const products = await getProducts({ isFeatured: true });
+	const billboard = await getBillboard(
+		"f5310f64-22da-49be-954c-b0bb3610a2f2"
+	);
+	return (
+		<Container>
+			<div className="space-y-10 pb-10">
+				<Billboard data={billboard} />
+			</div>
+			<div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+				<ProductList title="Featured Product" items={products} />
+			</div>
+		</Container>
+	);
 };
 
 export default HomePage;
